6a1f5e65698144dd4c138893f261c5ff9e7d610a
chriskelsey <chriskelsey.net@gmail.com>
2019-06-01 15:20:24 -0500

    Updating out-of-date packages

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -3,11 +3,11 @@ $(document).ready(function() {
 		'width'  : $(window).width(),
 		'height' : $(window).height()
 	});
+	$('.form-response').hide();
 });
 
 $('nav ul li a, .intro a').on('click',function (e) {
 	var div = $(this).attr('href');
-	console.log(div);
     $('html,body').animate({
         scrollTop: $(div).offset().top,
         duration: 'slow',
@@ -17,41 +17,41 @@ $('nav ul li a, .intro a').on('click',function (e) {
 });
 
 $('form').on('submit', function(e){
-	e.preventDefault();
+	//resetErrors();
 	$this = $(this);
-    var sendForm = formValidate($this);
-    console.log(sendForm);
+	var data = inputProcess();
+	$.ajax({
+		dataType: 'json',
+		type: 'POST',
+		url: $this.attr('action'),
+		data: data,
+		success : function(response){
+			console.log(data);
+			console.log(response);
+			if (!response) {
+                $this.hide();
+				$('.form-response').show();
+            } else {
+            	$.each(response, function(i, v) {
+	        		console.log(i + " => " + v); // view in console for error messages
+                	var msg = '<label class="error" for="'+i+'">'+v+'</label>';
+                	$('input[name="' + i + '"], textarea[name="' + i + '"]').addClass('inputError').after(msg);
+            	});
+              }
+		}
+	});
+	e.preventDefault();
 });
 
-function formValidate(form){
-	$('input').each(function(){
-		var returnVal = false;
- 		var input = $(this);
- 		var name = input.attr('name');
- 		//Checks for numbers not being in Name, this is due to me receiving a lot of spam with random strings in the Name column
-		var num = /([0-9])+/g;
-		//Email regular expression - 90/10 rule to prevent anything else being added in.	
-		var email= /^\S+@\S+\.\S+$/;
-		if (name === 'name' && input.val().match(num)){
-			alert('Please enter a proper name!');
-		} else if(name === 'email' && !input.val().match(email)){
-			alert('Please enter an email address');
-		} else if($('textarea').val() === ''){
-			alert('Please add a Message');
-		} else {
-			var returnVal = true;
-		}
-		/*else {
-			$.ajax({
-	       		type: 'POST',
-			    url: form.attr('action'),
-			    data: form.serialize(),
-			    success : function(data){
-					form.hide();
-					$('.contact-form').load('/thanks.html .form-response');
-			    }
-	    	});
-		}*/
-	return returnVal;
+function inputProcess(){
+	var data = {};
+	$('input, textarea').each(function(i,v){
+		data[v.name] = v.value;
 	});
+	return data;
+}
+
+function resetErrors(){
+	$('input, textarea').removeClass('inputError');
+	$('label.error').remove();
 }
