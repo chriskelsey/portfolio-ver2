129d80ab6e05dc5ebc02998371f87508d0ed2ec3
chriskelsey <chriskelsey.net@gmail.com>
2021-02-26 20:27:54 -0800

    Vanilla JS Nav animation with easing

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,12 +1,90 @@
-$(document).ready(function() {
-	$('.intro').css({
-		'width'  : $(window).width(),
-		'height' : $(window).height()
-	});
-	$('.form-response').hide();
+//Global Variables
+const intro = document.getElementById('intro');
+const navBar = document.querySelectorAll('nav ul a');
+const introLink = document.querySelector('#intro a');
+const formResponse = document.querySelector('.form-response');
+let num = 1;
+
+//Initialize DOM Styling functions
+function init() {
+  intro.style.width = document.body.offsetWidth + 'px';
+  intro.style.height = window.innerHeight + 'px';
+  if (formResponse) {
+    formResponse.style.display = 'none';
+  }
+}
+
+init();
+
+function animateScroll(item) {
+  const attr = item.getAttribute('href');
+  const pageDiv = document.getElementById(attr.slice(1));
+  const divLoc =
+    pageDiv.getBoundingClientRect().top +
+    window.pageYOffset -
+    document.documentElement.clientTop;
+  const scroll =
+    window.scrollY < divLoc
+      ? scrollDirection(divLoc, true)
+      : scrollDirection(divLoc, false);
+}
+
+function scrollDirection(el, op) {
+  const scrollInterval = setInterval(() => {
+    if (op && Math.round(window.scrollY) < Math.floor(el)) {
+      window.scrollBy(0, step(el, op));
+    } else if (!op && Math.round(window.scrollY) > Math.round(el)) {
+      window.scrollBy(0, step(el, op));
+    } else {
+      clearInterval(scrollInterval);
+      num = 1;
+    }
+  }, 1);
+}
+
+function step(el, op) {
+  if (!op) {
+    if (window.pageYOffset > el / 2) {
+      num++;
+      return (1 + ((1 - num) * (1 - num)) / 1000) * -1;
+    } else {
+      num--;
+      return (1 + ((1 - num) * (1 - num)) / 1000) * -1;
+    }
+  } else {
+    if (window.pageYOffset < el / 2) {
+      num++;
+      return 1 + ((1 - num) * (1 - num)) / 1000;
+    } else {
+      num--;
+      return 1 + ((1 - num) * (1 - num)) / 1000;
+    }
+  }
+}
+
+//Event listeners
+
+//navbar scroll click
+for (const link of navBar) {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+    animateScroll(link);
+  });
+}
+
+//Chrome remembers the location of the browser window - this will disable
+window.addEventListener('beforeunload', () => {
+  console.log('Unload Triggered');
+  window.scrollTo(0, 0);
 });
 
-$('nav ul li a, .intro a').on('click',function (e) {
+//Resize background on window size change
+window.addEventListener('resize', () => {
+  intro.style.width = document.body.offsetWidth + 'px';
+  intro.style.height = window.innerHeight + 'px';
+});
+
+/*$('nav ul li a, .intro a').on('click',function (e) {
 	var div = $(this).attr('href');
     $('html,body').animate({
         scrollTop: $(div).offset().top,
@@ -54,4 +132,4 @@ function inputProcess(){
 function resetErrors(){
 	$('input, textarea').removeClass('inputError');
 	$('label.error').remove();
-}
+}*/
--- server.js
+++ server.js
@@ -3,10 +3,6 @@ const path = require('path')
 const app = express();
 const PORT = 3001;
 
-/*app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, './client/build/index.html'));
-});*/
-
 app.use(express.static("public"));
 
 app.listen(PORT, () => {
